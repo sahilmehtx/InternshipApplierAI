fa307f19d26207476f20b00a12cf2a028a50ebee
sahilmehtx <smehta27@wisc.edu>
2024-05-20 17:42:09 -0500

    latest

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,12 @@
 const User = require('../models/User');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const generateToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: '30d',
+    });
+};
 
 exports.registerUser = async (req, res) => {
     const { name, email, password, skills, interests } = req.body;
@@ -12,7 +19,10 @@ exports.registerUser = async (req, res) => {
             interests,
         });
         await user.save();
-        res.status(201).json({ message: 'User registered successfully' });
+        res.status(201).json({ 
+            message: 'User registered successfully',
+            token: generateToken(user._id),
+        });
     } catch (error) {
         res.status(400).json({ error: error.message });
     }
@@ -23,7 +33,10 @@ exports.loginUser = async (req, res) => {
     try {
         const user = await User.findOne({ email });
         if (user && bcrypt.compareSync(password, user.password)) {
-            res.status(200).json({ message: 'Login successful' });
+            res.status(200).json({ 
+                message: 'Login successful',
+                token: generateToken(user._id),
+            });
         } else {
             res.status(400).json({ message: 'Invalid credentials' });
         }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const { registerUser, loginUser } = require('../controllers/userController');
+const { protect } = require('../middleware/auth');
 const router = express.Router();
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
+router.get('/profile', protect, (req, res) => {
+    res.json(req.user);
+});
 
 module.exports = router;
